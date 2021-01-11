264c757b561a4ead3deaab9248eaaf298a3fb13f
Jeremy <jeremy@jsrath.com>
2021-01-11 17:51:54 +0100

    refactor: add separate methods for different API calls

--- src/services/api-service.tsx
+++ src/services/api-service.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import fetch, { Headers } from 'node-fetch';
 import * as path from 'path';
 
-export function ApiService(option: string, trackingNumber: string): [Tracking[]] | [string] {
+export function ApiService(option: string, trackingNumber: string): [Tracking[]] {
   config({ path: path.join(__dirname, '../../.env') });
   const url = `${process.env.BASE_URL}`;
   const headers = new Headers({
@@ -16,68 +16,84 @@ export function ApiService(option: string, trackingNumber: string): [Tracking[]]
     },
   };
   const [data, setData] = useState<Tracking[]>([]);
-  const [apiData, setApiData] = useState<Tracking[]>(undefined);
-  const [error, setError] = useState<string>();
+  const [apiData, setApiData] = useState<Tracking[]>();
 
-  async function fetchPackages() {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers,
-    });
-    const json = await response.json();
-    setApiData(json.data.trackings);
+  function generateUrlWithSlug(): string {
+    const slug =
+      apiData?.length &&
+      apiData
+        .filter((tracking: any) => tracking.tracking_number === trackingNumber)
+        .map((tracking) => tracking.slug);
 
-    if (apiData && !apiData?.length && option !== "add") {
-      setError("Please add a package");
-    }
+    return slug ? `${url}/${slug}/${trackingNumber}` : undefined;
   }
 
-  async function fetchSinglePackage(method: string, hasBody: boolean) {
-    const slug = apiData?.length && apiData.filter((tracking: any) => tracking.tracking_number === trackingNumber).map((tracking) => tracking.slug);
-
-    const fetchUrl = slug ?  `${url}/${slug}/${trackingNumber}` : url;
-
-    const response = await fetch(fetchUrl, {
-      method,
+  async function addPackage() {
+    const response = await fetch(url, {
+      method: 'POST',
       headers,
-      ...(hasBody && { body: JSON.stringify(tracking) }),
+      body: JSON.stringify(tracking),
     });
 
-    console.log("CALLED SINGLE", await response.json())
+    const json = await response.json();
+    setData([json.data.tracking]);
+  }
 
-    //setData(await response.json());
+  async function getOrRemovePackage(isGetRequest: boolean) {
+    const fetchUrl = generateUrlWithSlug();
+
+    if (!fetchUrl) {
+      return;
+    }
+
+    const response = await fetch(fetchUrl, {
+      method: isGetRequest ? 'GET' : 'DELETE',
+      headers,
+    });
+
+    const json = await response.json();
+    setData([json.data.tracking]);
   }
 
   function returnAllPackages() {
-    if(!apiData) {
+    if (!apiData) {
       return;
     }
+
     setData(apiData);
   }
 
-  fetchPackages();
+  useEffect(() => {
+    async function fetchPackages() {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers,
+      });
+      const json = await response.json();
+      setApiData(json.data.trackings);
+    }
+    fetchPackages();
+  }, []);
 
   useEffect(() => {
-
     switch (option) {
       case 'add':
-        fetchSinglePackage('POST', true);
+        addPackage();
         break;
 
       case 'remove':
-        fetchSinglePackage('DELETE', false);
+        getOrRemovePackage(false);
         break;
 
       case 'trackingNumber':
-        console.log('CALLED WITH NUMBER');
-        fetchSinglePackage('GET', false);
+        getOrRemovePackage(true);
         break;
 
       default:
-        console.log('CALLED WITH NOTHING');
         returnAllPackages();
         break;
     }
   }, [apiData]);
-  return [error] || [data];
+
+  return [data];
 }
--- src/ui/ui.tsx
+++ src/ui/ui.tsx
@@ -2,27 +2,26 @@ import React from 'react';
 import Table from 'ink-table';
 import { Text } from 'ink';
 import { ApiService } from '../services/api-service';
-import { env } from 'process';
 
 const App = (cliOptions: CliOptions) => {
   const { trackingNumber } = cliOptions;
   const validOption = Object.keys(cliOptions).find((option) => cliOptions[option]);
   const [data] = ApiService(validOption, trackingNumber);
 
-  if (Array.isArray(data)) {
-    return data?.length ? (
-      <Table
-        data={data.map((tracking: any) => ({
-          tracking: tracking.tracking_number,
-          delivery: tracking.expected_delivery ?? tracking.shipment_delivery_date,
-        }))}
-      />
-    ) : (
-      <Text>Error, please add a package</Text>
-    );
-  } else {
-    return <Text>Loading...</Text>
+  function isDataValid(data: { id: string }[]): boolean {
+    return data.length && !!data[0].id;
   }
+
+  return isDataValid(data) ? (
+    <Table
+      data={data.map((tracking: any) => ({
+        tracking: tracking.tracking_number,
+        delivery: tracking.expected_delivery ?? tracking.shipment_delivery_date,
+      }))}
+    />
+  ) : (
+    <Text></Text>
+  );
 };
 
 module.exports = App;
